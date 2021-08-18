215f9cf457bf2b75ffbb2f98308b05ccf5875ec7
Crowdbotics <team@crowdbotics.com>
2021-08-18 14:35:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "jolly_band_29911Identifier",
+  url: "https://jolly_band_29911Identifier.botics.co",
+  api: "https://jolly_band_29911Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
